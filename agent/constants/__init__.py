d7d8cc4e830e7f90eefb12e01ec51f4030ed27ff
Rakesh Reddy <bhumireddyr@valuefy.com>
2025-11-15 20:05:31 +0530

    Added constants

---
+++ agent/constants/__init__.py
@@ -0,0 +1,13 @@
+CATEGORIES = [
+    "mail",
+    "calendar",
+    "drive",
+    "expense_tracker",
+    "resume_preparation",
+    "none"
+]
+
+# Default local model name (Ollama)
+DEFAULT_ROUTER_MODEL = "llama3"
+
+
